eb2f34f037ad9e4f56fbbe3b799b15480a9247a9
jsifalda <sifalda.jiri@gmail.com>
2017-05-26 15:44:26 +0200

    Use Set if available, version 2.0

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 module.exports = function (array) {
-  return array.filter(function (item, position, self) {
-    return self.indexOf(item) === position
-  })
+  var result = array
+  if (typeof Set !== 'undefined') {
+    result = Array.from(new Set(array))
+  } else {
+    result = array.filter(function (item, position, self) {
+      return self.indexOf(item) === position
+    })
+  }
+
+  return result
 }
